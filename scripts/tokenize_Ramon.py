fc69fb4f822f71e4c14517cb844f022ee3f19771
RamonReilman <gmodlovver@gmail.com>
2025-12-07 18:48:44 +0100

    Updates scripts to contains functionality to write tokens back to txt

--- scripts/tokenize_Ramon.py
+++ scripts/tokenize_Ramon.py
@@ -8,29 +8,39 @@ def to_path(path):
         return pathed
     raise FileNotFoundError(path)
 
+
 def setup_parser():
     parser = argparse.ArgumentParser(
-        prog = "Tokenize",
+        prog="Tokenize",
         description="For all your tokenizing needs",
         epilog="help"
     )
-    subparser = parser.add_subparsers(dest = "subcommand")
+    subparser = parser.add_subparsers(dest="subcommand")
     enc_gen = subparser.add_parser("generate-enc", description="Will generate a .enc file based on txt file input")
     enc_gen.add_argument("--txt_file", type=to_path,
-                        help = "Path to input .txt file", required=True)
-    enc_gen.add_argument("-o", "--output", default="./output.enc", help = "Path to generate an encoding file")
+                         help="Path to input .txt file", required=True)
+    enc_gen.add_argument("-o", "--output", default="./output.enc", help="Path to generate an encoding file")
     enc_gen.add_argument("-m", "--max_merges", default=10, help="Maximum amount of merges to be generated", type=int)
 
     tok_gen = subparser.add_parser("generate-toc", description="Will generate a tokens for a text file using encoding")
     tok_gen.add_argument("--txt_file", type=to_path,
-                        help = "Path to input .txt file", required=True)
+                         help="Path to input .txt file", required=True)
     tok_gen.add_argument("--enc_file", type=to_path,
-                        help = "Path to input .enc file", required=True)
-    tok_gen.add_argument("-o", "--output", default="./output.tok", help = "Path to generate a tok file")
+                         help="Path to input .enc file", required=True)
+    tok_gen.add_argument("-o", "--output", default="./output.tok", help="Path to generate a tok file")
+
+    txt_gen = subparser.add_parser("generate-txt",
+                                   description="Will generate a text file based on tokens and .enc file")
+    txt_gen.add_argument("--enc_file", type=to_path,
+                         help="Path to input .enc file", required=True)
+    txt_gen.add_argument("--tok_file", type=to_path,
+                         help="Path to input .tok file", required=True)
+    txt_gen.add_argument("-o", "--output", default="./output.txt", help="Path to generate a txt file")
     return parser
 
+
 def read_file(path):
-    file = [] if path.suffix == ".txt" else {}
+    file = [] if path.suffix in (".txt", ".tok") else {}
     with open(path, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
@@ -40,11 +50,16 @@ def read_file(path):
             if path.suffix == ".enc":
                 line_split = line.split(" ")
                 file[(line_split[0], line_split[1])] = line_split[-1]
+            if path.suffix == ".tok":
+                line_split = line.split(" ")
+                file.append(line_split)
     return file
+
 
 def init_vocab(corpus):
     vocab = set(char for word in corpus for char in word)
     return vocab
+
 
 def init_freq(corpus):
     vocab = {}
@@ -55,15 +70,16 @@ def init_freq(corpus):
             vocab[letter] += 1
     return vocab
 
+
 def find_highest_pair(corpus):
     pairs_count = {}
 
     for sentence in corpus:
         n_sentence = len(sentence)
-        for i in range(0, n_sentence-1):
+        for i in range(0, n_sentence - 1):
 
             first = sentence[i]
-            second = sentence[i+1]
+            second = sentence[i + 1]
             combined = (first, second)
             if first == ' ' or second == ' ':
                 continue
@@ -77,20 +93,21 @@ def find_highest_pair(corpus):
     highest_pair = max(pairs_count.items(), key=lambda x: x[1])[0]
     return highest_pair, highest_pair_val
 
+
 def update_freq(freq, highest_pair):
     pair = highest_pair[0]
     freq[f"{pair[0]}{pair[1]}"] = highest_pair[1]
     for i in range(0, highest_pair[1]):
         for j in range(0, 2):
             try:
-                freq[pair[j]]-=1
+                freq[pair[j]] -= 1
                 if freq[pair[j]] == 0:
                     freq.pop(pair[j])
             except Exception as _:
                 continue
 
-
     return freq
+
 
 def update_corpus(corpus, highest_pair):
     highest_pair = highest_pair[0]
@@ -102,19 +119,21 @@ def update_corpus(corpus, highest_pair):
         i = 0
         temp_sentence = []
         while i < n:
-            if i < n-1 and ((sentence[i],sentence[i+1]) == highest_pair):
+            if i < n - 1 and ((sentence[i], sentence[i + 1]) == highest_pair):
 
-                temp_sentence.append(f"{sentence[i]}{sentence[i+1]}")
-                i+=2
+                temp_sentence.append(f"{sentence[i]}{sentence[i + 1]}")
+                i += 2
             else:
                 temp_sentence.append(sentence[i])
-                i+=1
+                i += 1
         new_corpus.append(temp_sentence)
     return new_corpus
+
 
 def update_vocab(new_token, vocab):
     vocab.add(f"{new_token[0][0]}{new_token[0][1]}")
     return vocab
+
 
 def write_enc(merges, output):
     output = pathlib.Path(output)
@@ -124,6 +143,7 @@ def write_enc(merges, output):
         for merge1, merge2 in merges:
             f.write(f"{merge1} {merge2} {merge1}{merge2}\n")
 
+
 def write_tok(tokens, output):
     output = pathlib.Path(output)
     if output.is_dir():
@@ -131,7 +151,8 @@ def write_tok(tokens, output):
 
     with open(output, "w", encoding="utf-8") as f:
         for sentence in tokens:
-            f.write(" ".join(sentence))
+            f.write(" ".join(sentence) + "\n")
+
 
 def generate_enc(args):
     corpus = read_file(args.txt_file)
@@ -147,8 +168,9 @@ def generate_enc(args):
         freq = update_freq(freq, highest_pair)
         vocab = update_vocab(highest_pair, vocab)
         merges.append(highest_pair[0])
-        tries+=1
+        tries += 1
     write_enc(merges, args.output)
+
 
 def update_tokens_with_mergerules(corpus, rules):
     new_corpus = []
@@ -160,29 +182,69 @@ def update_tokens_with_mergerules(corpus, rules):
             i = 0
             new_sentence = []
             while i < n:
+                if i < n - 1:
+                    merge = get_merge(rules, sentence[i], sentence[i+1])
+                    if merge:
+                        new_sentence.append(rules.get((sentence[i], sentence[i + 1])))
+                        i += 2
+                        changed = True
+                        continue
 
-                if i < n-1 and rules.get((sentence[i], sentence[i+1])) is not None:
-                    current_pair = (sentence[i], sentence[i + 1])
-                    new_sentence.append(rules.get(current_pair))
-                    i += 2
-                    changed = True
-                else:
-                    new_sentence.append(sentence[i])
-                    i+=1
+                new_sentence.append(sentence[i])
+                i += 1
             sentence = new_sentence
         new_corpus.append(new_sentence)
     return new_corpus
 
+def get_merge(rules, s1, s2):
+    pair = (s1,s2)
+    if s1 in rules.keys():
+        return pair
+    elif pair in rules.keys():
+        return rules.get(pair)
+    return None
+
+
+
 def generate_toc(args):
     corpus = read_file(args.txt_file)
-    print(f"Corpus: {corpus}")
     enc = read_file(args.enc_file)
-    print(f"rules: {enc}")
     tokens = update_tokens_with_mergerules(corpus, enc)
-    print(f"Tokens: {tokens}")
     write_tok(tokens, args.output)
 
-    # generate tokens based on enc
+def write_txt(text, output):
+    output = pathlib.Path(output)
+    if output.is_dir():
+        output = output / "output.txt"
+    with open(output, "w", encoding="utf-8") as f:
+        for line in text:
+            f.write("".join(line).replace("_", " ") + "\n")
+
+
+def generate_txt(args):
+    enc = read_file(args.enc_file)
+    enc = {v: k for k, v in enc.items()}
+    tokens = read_file(args.tok_file)
+    n = []
+    for sentence in tokens:
+        changed = True
+        while changed:
+            changed = False
+            new_sentence = []
+
+            for tok in sentence:
+                if tok in enc:
+                    left, right = enc[tok]
+                    new_sentence.extend([left, right])
+                    changed = True
+                else:
+                    new_sentence.append(tok)
+
+            sentence = new_sentence
+        n.append(sentence)
+    write_txt(n, args.output)
+
+
 
 def main():
     args = setup_parser().parse_args()
@@ -190,6 +252,9 @@ def main():
         generate_enc(args)
     elif args.subcommand == "generate-toc":
         generate_toc(args)
+    elif args.subcommand == "generate-txt":
+        generate_txt(args)
+
 
 if __name__ == "__main__":
     main()
